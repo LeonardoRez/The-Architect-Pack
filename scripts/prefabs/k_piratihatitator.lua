6b4ebf185d729c0843a5b5a0cbe97d3025f64171
Kynoox_ <40477759+ImKyno@users.noreply.github.com>
2020-02-12 18:42:29 -0300

    Create k_piratihatitator.lua
    New Prefab Piratihatitator.

---
+++ scripts/prefabs/k_piratihatitator.lua
@@ -0,0 +1,70 @@
+require "prefabutil"
+
+local assets =
+{
+	Asset("ANIM", "anim/dragoon_den.zip"),
+	Asset("IMAGE", "images/minimapimages/kyno_minimap_atlas_sw.tex"),
+	Asset("ATLAS", "images/minimapimages/kyno_minimap_atlas_sw.xml"),
+}
+
+local function onhammered(inst, worker)
+	inst.components.lootdropper:DropLoot()
+	SpawnPrefab("collapse_big").Transform:SetPosition(inst.Transform:GetWorldPosition())
+	inst.SoundEmitter:PlaySound("dontstarve/common/destroy_stone")
+	inst:Remove()
+end
+
+local function onhit(inst, worker)
+	inst.AnimState:PlayAnimation("hit")
+	inst.AnimState:PushAnimation("idle")
+end
+
+local function onbuilt(inst)
+	inst.AnimState:PlayAnimation("place")
+	inst.AnimState:PushAnimation("idle")
+end
+
+local function fn()
+	local inst = CreateEntity()
+    
+	inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
+	inst.entity:AddNetwork()
+
+	local minimap = inst.entity:AddMiniMapEntity()
+	minimap:SetIcon("dragoon_den.png")
+	
+	inst.AnimState:SetBank("dragoon_den")
+	inst.AnimState:SetBuild("dragoon_den")
+	inst.AnimState:PlayAnimation("idle", true)
+	
+	MakeObstaclePhysics(inst, 1.5)
+	
+	inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+	inst:AddComponent("inspectable")
+	
+	inst:AddComponent("hauntable")
+    inst.components.hauntable:SetHauntValue(TUNING.HAUNT_TINY)
+	
+	inst:AddTag("structure")
+	
+    inst:AddComponent("lootdropper")
+    inst:AddComponent("workable")
+    inst.components.workable:SetWorkAction(ACTIONS.HAMMER)
+	inst.components.workable:SetOnFinishCallback(onhammered)
+	inst.components.workable:SetOnWorkCallback(onhit)
+	inst.components.workable:SetWorkLeft(3)
+
+	inst:ListenForEvent("onbuilt", onbuilt)
+
+	return inst
+end
+
+return Prefab("kyno_dragoonden", fn, assets, prefabs),
+MakePlacer("kyno_dragoonden_placer", "dragoon_den", "dragoon_den", "idle")  
