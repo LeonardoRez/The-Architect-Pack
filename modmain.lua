6eb97a21c56f7b01b38ac5e03f3f6527e6053620
Kynoox_ <40477759+ImKyno@users.noreply.github.com>
2020-04-14 13:41:21 -0300

    CC Changes
    HE CANT STOP

--- modmain.lua
+++ modmain.lua
@@ -2162,9 +2162,9 @@ local DST = GLOBAL.TheSim:GetGameID() == "DST"
 				setval(v, "OnOverrideCCTable.UpdateAmbientCCTable.SEASON_COLOURCUBES",{
 					autumn =
 					{
-						day = resolvefilepath("images/colour_cubes/pork_warm_day_cc.tex"),
-						dusk = resolvefilepath("images/colour_cubes/pork_warm_day_cc.tex"),
-						night = resolvefilepath("images/colour_cubes/pork_warm_night_cc.tex"),
+						day = resolvefilepath("images/colour_cubes/sw_mild_day_cc.tex"),
+						dusk = resolvefilepath("images/colour_cubes/sw_mild_day_cc.tex"),
+						night = resolvefilepath("images/colour_cubes/SW_mild_night_cc_cc.tex"),
 						full_moon = resolvefilepath("images/colour_cubes/pork_cold_bloodmoon_cc.tex"),
 					},
 					winter =
